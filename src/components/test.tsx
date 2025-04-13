ebdbf3985ca239c3d7024ffd9bb5c6bee79e99f5
TatsukiSatoh <tssa.0927@gmail.com>
2025-04-13 17:16:47 +0900

    i18nの設定がlocalで動作することを確認

---
+++ src/components/test.tsx
@@ -0,0 +1,10 @@
+import {useTranslation} from "react-i18next"
+
+export default function Test() {
+    const {t} = useTranslation()
+    return (
+        <div>
+            <h1>{t("welcome")}</h1>
+        </div>
+    )
+}
